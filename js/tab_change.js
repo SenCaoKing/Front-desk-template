f0e7ece5b838d2ca011b51424ef0d344b0dd53c1
SenCaoKing <1134349927@qq.com>
2018-04-11 23:07:53 +0800

    添加tab_change.js

--- js/tab_change.js
+++ js/tab_change.js
@@ -1,2 +1,28 @@
+$(function(){
+	$(".log>li>a").hover(function(){
+		$(this).addClass("shouye");
+	},function(){
+		$(this).removeClass("shouye");
+		$("#sy").addClass("shouye");
+	})
+	$(".small li a").click(function(){
+		$(this).addClass("all").siblings().removeClass("all");
+		var index = $(this).index();
+		var newUl=$(this).parents('.cosplay').siblings('.tt').find('ul');
+		newUl.eq(index).addClass('active').siblings().removeClass('active');
+	})
+	$('.small li a:eq(0)').click(function(){
+		// $('.tt .tabchange').addClass('active');
+	})
+	$('.answer a').click(function(){
+		var n=$(this).index();
+		var e=$(this).parents('.cosplay').siblings('.clothes').find('.que');
+		e.eq(n).addClass('active').siblings().removeClass('active');
+	});
 
+
+
+
+	
+})
 
